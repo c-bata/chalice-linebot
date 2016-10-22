256b10484cc8468e1beb5d410011e135256c155d
c-bata <contact@c-bata.link>
2016-10-22 14:38:16 +0900

    Refactor

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ import random
 
 from chalice import Chalice
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import CarouselColumn
-from linebot.models import CarouselTemplate
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage,
+    CarouselColumn, CarouselTemplate, URITemplateAction,
+)
 import requests
-from linebot.models import MessageTemplateAction
-from linebot.models import PostbackTemplateAction
-from linebot.models import URITemplateAction
 
 app = Chalice(app_name='linebot')
 line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
@@ -107,47 +105,19 @@ def _today_news(msg):
         return
 
     carousel_template_message = TemplateSendMessage(
-        alt_text="Today's news. now loading...",
+        alt_text="今日のニュース\nこのメッセージが見えている端末ではこの機能に対応していません。",
         template=CarouselTemplate(columns=[
             CarouselColumn(
                 thumbnail_image_url='https://s3-us-west-2.amazonaws.com/lineapitest/hamburger_240.jpeg',
                 title='this is menu1',
                 text='description1',
-                actions=[
-                    PostbackTemplateAction(
-                        label='postback1',
-                        text='postback text1',
-                        data='action=buy&itemid=1'
-                    ),
-                    MessageTemplateAction(
-                        label='message1',
-                        text='message text1'
-                    ),
-                    URITemplateAction(
-                        label='uri1',
-                        uri='http://example.com/1'
-                    )
-                ]
+                actions=[URITemplateAction(label='詳細を見る', uri='http://example.com/1')]
             ),
             CarouselColumn(
                 thumbnail_image_url='https://s3-us-west-2.amazonaws.com/lineapitest/hamburger_240.jpeg',
                 title='this is menu2',
                 text='description2',
-                actions=[
-                    PostbackTemplateAction(
-                        label='postback2',
-                        text='postback text2',
-                        data='action=buy&itemid=2'
-                    ),
-                    MessageTemplateAction(
-                        label='message2',
-                        text='message text2'
-                    ),
-                    URITemplateAction(
-                        label='uri2',
-                        uri='http://example.com/2'
-                    )
-                ]
+                actions=[URITemplateAction(label='詳細を見る', uri='http://example.com/2')]
             )
         ])
     )
